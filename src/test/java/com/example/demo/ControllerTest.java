40668b2c6c724b3de70882548803ba129d41f639
Tomasz Borowski <bortom112233@gmail.com>
2022-05-06 14:23:07 +0200

    Initial commit

---
+++ src/test/java/com/example/demo/ControllerTest.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.shadow.com.univocity.parsers.conversions.Conversions.string;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void shouldShowDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().is2xxSuccessful()).andExpect(content().string("eluwina Tommo"));
+    }
+
+}
